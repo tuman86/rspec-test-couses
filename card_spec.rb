9f2043c58772870fe3cfb49d502ed9b08b241f9e
Som <smdtt86@gmail.com>
2021-06-28 15:19:20 +0530

    First spec test to check rank and suits

---
+++ card_spec.rb
@@ -0,0 +1,19 @@
+class Card
+  attr_reader :type
+
+  def initialize(type)
+    @type = type
+  end
+end
+
+RSpec.describe Card do
+  it 'has a rank' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.suit).to eq('Spades')
+  end
+end

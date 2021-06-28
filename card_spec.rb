74f96f15e5751c7daf922d58ea6110b06387763f
Som <smdtt86@gmail.com>
2021-06-28 16:12:01 +0530

    reduced duplication with helper methods

--- card_spec.rb
+++ card_spec.rb
@@ -8,15 +8,15 @@ class Card
 end
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spades')
+  def card
+    Card.new('Ace', 'Spades')
   end
 
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 end

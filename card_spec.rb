c5fccbc9880302f22edc896c2129339d5c41483b
Som <smdtt86@gmail.com>
2021-06-28 16:43:23 +0530

    Custom error mesages

--- card_spec.rb
+++ card_spec.rb
@@ -21,4 +21,10 @@ RSpec.describe Card do
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
   end
+
+  it 'has a custom error message' do
+    card.suit = 'Nonsense'
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
+  end
 end
